26fd25013f09236f6646a25cab348da4a8347d1e
Min Nam <code.minnam@gmail.com>
2022-02-12 14:35:13 +0000

    Add router and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
 
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  return <div className="App">Hello WIP group!</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
