adffe99900ae0ce288b14dc6af798fd75fccc9ef
Min Nam <code.minnam@gmail.com>
2022-02-12 23:49:32 +0000

    Add filter routing

--- src/components/explore/Header.tsx
+++ src/components/explore/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Header.css';
 import TuneIcon from '@mui/icons-material/Tune';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const openFilter = () => navigate('/filter');
+
   return (
     <header className="ExploreHeaderContainer">
       <h1>Shoreditch, London</h1>
-      <TuneIcon />
+      <TuneIcon onClick={openFilter} />
     </header>
   );
 };
