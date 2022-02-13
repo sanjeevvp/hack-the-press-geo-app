eb7c5a26f3537157a86286e9d0e1cf9c494fdc1b
Muhammad Qasim <qasim9872@gmail.com>
2022-02-13 11:19:25 +0000

    added theme to app and updated default value for radius

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 import Explore from './pages/Explore';
 import Filter from './pages/Filter';
@@ -10,15 +11,38 @@ import PostDetail from './pages/PostDetail';
 import AppProvider from './contexts/app.context';
 
 const App = () => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#F8A82F',
+        dark: '#F8A82F',
+      },
+      success: {
+        main: '#9BF8D6',
+        dark: '#9BF8D6',
+      },
+      info: {
+        main: '#FD93FF',
+        dark: '#FD93FF',
+      },
+      secondary: {
+        main: '#D9B4FF',
+        dark: '#D9B4FF',
+      },
+    },
+  });
+
   return (
-    <AppProvider>
-      <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/post/:postId" element={<PostDetail />} />
-        <Route path="/post" element={<NewPost />} />
-        <Route path="/filter" element={<Filter />} />
-      </Routes>
-    </AppProvider>
+    <ThemeProvider theme={theme}>
+      <AppProvider>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/post/:postId" element={<PostDetail />} />
+          <Route path="/post" element={<NewPost />} />
+          <Route path="/filter" element={<Filter />} />
+        </Routes>
+      </AppProvider>
+    </ThemeProvider>
   );
 };
 
