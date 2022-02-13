d1f50be9a25046917c92441ba1c3cdd446d8e7a3
Min Nam <code.minnam@gmail.com>
2022-02-13 15:27:09 +0000

    Add style on filter header

--- src/components/filter/Header.tsx
+++ src/components/filter/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Header.css';
+import CloseIcon from '@mui/icons-material/Close';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const closeFilter = () => navigate('/');
+
   return (
-    <header className="HeaderContainer">
-      <h1>Filter your posts</h1>
+    <header className="FilterHeaderContainer">
+      <h1 style={{ fontSize: '24px', fontWeight: '900' }}>Filter your posts</h1>
+      <CloseIcon onClick={closeFilter} />
     </header>
   );
 };
